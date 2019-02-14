a7b89e1cbe56e68084b68c3829d0c75da3d9036a
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-14 23:49:39 +0530

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -23,16 +23,14 @@ Label memLabel=new Label(" ",Label.RIGHT);
 final int FRAME_WIDTH=325,FRAME_HEIGHT=325;  
 final int HEIGHT=30, WIDTH=30, H_SPACE=10,V_SPACE=10;  
 final int TOPX=30, TOPY=50; 
-MyCalculator(String frameText)//constructor  
+MyCalculator(String frameText)  
 {  
-super(frameText);  
-  
+super(frameText);   
 int tempX=TOPX, y=TOPY;  
 displayLabel.setBounds(tempX,y,240,HEIGHT);  
 displayLabel.setBackground(Color.BLUE);  
 displayLabel.setForeground(Color.WHITE);  
 add(displayLabel);  
-  
 memLabel.setBounds(TOPX,  TOPY+HEIGHT+ V_SPACE,WIDTH, HEIGHT);  
 add(memLabel);   
 tempX=TOPX;   
